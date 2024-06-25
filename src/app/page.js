b50bcb5f507e1ac70758ab3b0acf8ae19a16be91
Md. Jamal Uddin <jaamaalxyz@gmail.com>
2024-06-25 11:14:24 +0600

    add logo and bengali fonts

--- src/app/page.js
+++ src/app/page.js
@@ -5,10 +5,27 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.center}>
+        <Image
+          className={styles.logo}
+          src="/logo.png"
+          alt="Demystifying Lab Logo"
+          width={75}
+          height={75}
+          priority
+          style={{
+            objectFit: 'contain',
+            objectPosition: 'center',
+            margin: '1rem 0',
+            filter: 'invert(1) drop-shadow(0 0 0.3rem #ffffff70)',
+            borderRadius: '50%',
+          }}
+        />
         <h1 className={styles.title}>Demystifying Lab</h1>
         <h3 className={styles.description}>
           Demystify Anything and Everything
         </h3>
+        <div style={{ padding: '1rem 0' }} />
+        <h2>এখন সবকিছু শেখা হবে আরও সহজে</h2>
       </div>
     </main>
   );
